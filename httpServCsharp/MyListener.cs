9fcae3f79f639d8bba18a25c947c372c83e6b56b
Masafir <luffyex@hotmail.fr>
2020-05-05 10:15:16 +0200

    first commit with api console basic request

---
+++ httpServCsharp/MyListener.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace httpServCsharp
+{
+    public class MyListener
+    {
+        public static void startListener(List<string> prefixes)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Pas supporté !");
+                return;
+            }
+
+            // Creation du listener
+            HttpListener listener = new HttpListener();
+            // Ajouts des uri
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Console.WriteLine("Listening...");
+            while (true)
+            {
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+
+                HttpListenerRequest request = context.Request;
+
+                string documentContents;
+                using (Stream receiveStream = request.InputStream)
+                {
+                    using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                    {
+                        documentContents = readStream.ReadToEnd();
+                    }
+                }
+                Console.WriteLine($"On a reçu une requête de : {request.Url}");
+                Console.WriteLine(documentContents);
+
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string responseString = "<HTML><BODY> Bienvenue sur la page d'accueil !</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+            }
+            listener.Stop();
+        }
+    }
+}
---
+++ httpServCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace httpServCsharp
+{
+    class Program
+    {
+ 
+ 
+        static void Main(string[] args)
+        {
+            var prefixes = new List<string>() { "http://*:8080/" };
+            MyListener.startListener(prefixes);
+        }
+    }
+}
